527b400d8654cd801c6cea0161b4be7ab32f682d
FxMathSolution <48218655+FxMathSolution@users.noreply.github.com>
2023-05-07 23:22:44 +0330

    Update provider.py

--- Copier/tradecopier/provider.py
+++ Copier/tradecopier/provider.py
@@ -1,65 +1,125 @@
-from datetime import datetime
-import json
-import requests
-from django.http import HttpResponseBadRequest,HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from .models import copier_orders, copier_accounts
+from django.http import HttpResponse
+from myapp.models import ProviderDeal  # Replace with your Django model for provider deals
 
-
-@csrf_exempt
 def provider(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        
-        # Get account information
-        account = copier_accounts.objects.get(Token=data['token'])
-        
-        # Process orders
-        for order in data['orders']:
-            try:
-                existing_order = copier_orders.objects.get(OrderID=order['orderId'])
-                
-                # Update existing order
-                existing_order.SL = order['stopLoss']
-                existing_order.TP = order['takeProfit']
-                existing_order.ClosePrice = order['closePrice']
-                existing_order.Swap = order['swap']
-                existing_order.Profit = order['profit']
-                existing_order.Commission = order['commission']
-                existing_order.IsClosed = order['isClosed']
-                existing_order.IsDeleted = order['isDeleted']
-                existing_order.IsUpdated = 1
-                existing_order.IsSynced = 1
-                existing_order.ModifiedDate = datetime.now()
-                existing_order.save()
-            except copier_orders.DoesNotExist:
-                # Create new order
-                new_order = copier_orders(
-                    OrderID=order['orderId'],
-                    OrderType=order['orderType'],
-                    Symbol=order['symbol'],
-                    Lots=order['lots'],
-                    OpenPrice=order['openPrice'],
-                    SL=order['stopLoss'],
-                    TP=order['takeProfit'],
-                    ClosePrice=order['closePrice'],
-                    Swap=order['swap'],
-                    Profit=order['profit'],
-                    Commission=order['commission'],
-                    Comment=order['comment'],
-                    MagicNumber=order['magicNumber'],
-                    OrderTime=datetime.fromtimestamp(order['openTime']),
-                    CloseTime=datetime.fromtimestamp(order['closeTime']),
-                    AccountID=account.id,
-                    IsClosed=order['isClosed'],
-                    IsDeleted=order['isDeleted'],
-                    IsUpdated=1,
-                    IsSynced=1,
-                    CreatedDate=datetime.now(),
-                    ModifiedDate=datetime.now()
-                )
-                new_order.save()
-        
-        return HttpResponse('OK')
+    # Get parameters from the request
+    status = int(request.GET.get('status', '-1'))
+    provider = request.GET.get('provider')
+    broker = request.GET.get('broker')
+    account_no = request.GET.get('account_no')
+    ticket = request.GET.get('ticket')
+    ip = request.GET.get('ip', '')[:50]
+    computer = request.GET.get('computer')
+    delay = request.GET.get('delay')
+
+    # Process the request based on the status parameter
+    if status == 1:
+        symbol = request.GET.get('symbol')
+        type_ = request.GET.get('type')
+        lot = request.GET.get('lot')
+        balance = request.GET.get('balance')
+        equity = request.GET.get('equity')
+        open_time = request.GET.get('open_time')
+        open_price = request.GET.get('open_price')
+        sl = request.GET.get('sl')
+        tp = request.GET.get('tp')
+
+        # Save the provider deal to the database
+        deal = ProviderDeal.objects.create(
+            provider=provider,
+            broker=broker,
+            account_no=account_no,
+            ticket=ticket,
+            symbol=symbol,
+            type_=type_,
+            lot=lot,
+            balance=balance,
+            equity=equity,
+            open_time=open_time,
+            open_price=open_price,
+            sl=sl,
+            tp=tp,
+            ip=ip,
+            computer=computer,
+            delay=delay,
+        )
+
+    elif status == 2:
+        close_price = request.GET.get('close_price')
+        profit = request.GET.get('profit')
+        commission = request.GET.get('commission')
+        swap = request.GET.get('swap')
+
+        # Update the provider deal in the database
+        deal = ProviderDeal.objects.filter(
+            provider=provider,
+            broker=broker,
+            account_no=account_no,
+            ticket=ticket,
+        ).update(
+            close_price=close_price,
+            profit=profit,
+            commission=commission,
+            swap=swap,
+            ip=ip,
+            computer=computer,
+            delay=delay,
+        )
+
+    elif status == 3:
+        # Delete the provider deal from the database
+        ProviderDeal.objects.filter(
+            provider=provider,
+            broker=broker,
+            account_no=account_no,
+            ticket=ticket,
+        ).delete()
+
+    elif status == 4:
+        open_price = request.GET.get('open_price')
+        sl = request.GET.get('sl')
+        tp = request.GET.get('tp')
+
+        # Update the provider deal in the database
+        deal = ProviderDeal.objects.filter(
+            provider=provider,
+            broker=broker,
+            account_no=account_no,
+            ticket=ticket,
+        ).update(
+            open_price=open_price,
+            sl=sl,
+            tp=tp,
+            ip=ip,
+            computer=computer,
+            delay=delay,
+        )
+
+    elif status == 5:
+        old_ticket = request.GET.get('old_ticket')
+        lot = request.GET.get('lot')
+        old_lot = request.GET.get('old_lot')
+
+        # Save the suborder to the database
+        deal = ProviderDeal.objects.create(
+            provider=provider,
+            broker=broker,
+            account_no=account_no,
+            ticket=ticket,
+            old_ticket=old_ticket,
+            lot=lot,
+            old_lot=old_lot,
+            ip=ip,
+            computer=computer,
+            delay=delay,
+        )
+
     else:
-        return HttpResponseBadRequest()
+        # Invalid status parameter
+        return HttpResponse('-1<br>invalid parameter format')
+
+    # Return the response
+    if deal:
+        return HttpResponse(f'{deal.id}<br>succeeded')
+    else:
+        return HttpResponse(f'{deal}<br>{error}')
